ef41be5e17ca4164c329ac1b7e242cc4fcb87c7e
HeshanHH <heshanwijerathna01@gmail.com>
2019-09-13 17:04:01 +0530

    Dicee app

--- app/src/main/java/com/hdev/dicee/MainActivity.java
+++ app/src/main/java/com/hdev/dicee/MainActivity.java
@@ -3,6 +3,10 @@ package com.hdev.dicee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button rollButton;//only can hold numbers
+        rollButton = (Button) findViewById(R.id.rollButton);//telling rollbutton is indeed a button (Button)
+        ImageView rightDice = (ImageView) findViewById(R.id.image_rightDice);
+        ImageView lefttDice = (ImageView) findViewById(R.id.image_leftDice);
+
+
+        //Listener to button
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d("Dicee","Ze button hase been pressed!! ");//
+
+
+            }
+        });
+
+
+
     }
 }
